359d5469cebd04634dbc579876648106967ccab8
Jonathan Casillas <react.jonathan.casillas@gmail.com>
2021-07-14 18:08:33 -0500

    se Agrego la pantalla ChangeThemeScreen.tsx

--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -1,5 +1,5 @@
-import { useNavigation } from '@react-navigation/native';
 import React from 'react'
+import { useNavigation, useTheme } from '@react-navigation/native';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'
 import { MenuItems } from '../interfaces/appInterfaces';
@@ -12,30 +12,34 @@ interface Props {
 const FlatListMenuItem = ({ menuItem }: Props) => {
 
     const navigation = useNavigation();
+    const { colors } = useTheme();
 
 
     return (
         <TouchableOpacity
-            activeOpacity={ 0.8 }
-            onPress={() => navigation.navigate(menuItem.component) }
+            activeOpacity={0.8}
+            onPress={() => navigation.navigate(menuItem.component)}
         >
             <View style={styles.container} >
-    
+
                 <Icon name={menuItem.icon}
                     color='#5856D6'
                     size={30}
                 />
-                <Text style={styles.itemText} >
+                <Text style={{
+                    ...styles.itemText,
+                    color: colors.text
+                    }} >
                     {menuItem.name}
                 </Text>
-    
-                <View style={{flex: 1 }} />
+
+                <View style={{ flex: 1 }} />
                 <Icon name='chevron-forward-outline'
                     color='#5856D6'
                     size={30}
                 />
 
-        </View>
+            </View>
         </TouchableOpacity>
 
 
