cea42aa9ddc979f38d21038f9a5a4b27a276154b
Jonathan Casillas <react.jonathan.casillas@gmail.com>
2021-07-16 15:52:49 -0500

    Se añaden los archivos ThemeContext.tsx y ThemeReducer.tsx y acambios en el archivo ChangeThemeScreen.tsx

---
+++ src/screens/ChangeThemeScreen.tsx
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import HeaderTitle from '../components/HeaderTitle';
+import { ThemeContext } from '../context/themeContext/ThemeContex';
+import { styles } from '../Theme/appTheme';
+
+const ChangeThemeScreen = () => {
+
+    const { setDarkTheme, setLightTheme, theme: { colors } } = useContext(ThemeContext)
+
+    return (
+        <View style={styles.globalMargin} >
+            <HeaderTitle title='Theme' />
+
+            <View style={{flexDirection: 'row', justifyContent: 'space-between' }} >
+
+
+                <TouchableOpacity
+                    onPress={setLightTheme}
+                    style={{
+                        backgroundColor: colors.primary,
+                        justifyContent: 'center',
+                        width: 150,
+                        height: 50,
+                        borderRadius: 15,
+                    }}
+                    activeOpacity={0.8}
+                >
+                    <Text style={{
+                        color: 'white',
+                        textAlign: 'center',
+                        fontSize: 22,
+                    }} > Light </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={setDarkTheme}
+                    style={{
+                        backgroundColor: colors.primary,
+                        justifyContent: 'center',
+                        width: 150,
+                        height: 50,
+                        borderRadius: 15,
+                    }}
+                    activeOpacity={0.8}
+                >
+                    <Text style={{
+                        color: 'white',
+                        textAlign: 'center',
+                        fontSize: 22,
+                    }} > Dark </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+export default ChangeThemeScreen;
